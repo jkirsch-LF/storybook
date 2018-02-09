b98bafd55c92eec3275df2b2e2513b56b864fd9a
James Kirsch <jameskirschaf@gmail.com>
2018-02-08 22:10:48 -0800

    added user model

--- app.js
+++ app.js
@@ -2,11 +2,24 @@ const express = require('express');
 const mongoose = require('mongoose');
 const passport = require('passport');
 
+// Load User Model
+require('./models/User');
+
 // Passport Config
 require('./config/passport')(passport);
 
 // Load Routes
 const auth = require('./routes/auth');
+
+// Load Keys
+const keys = require('./config/keys');
+
+// Map global promises
+mongoose.Promise = global.Promise;
+// Mongoose Connect
+mongoose.connect(keys.mongoURI)
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
 
 const app = express();
 
