ff3c941dd474115df320d92aa16b9d480f4d648d
James Kirsch <jameskirschaf@gmail.com>
2018-02-27 13:45:23 -0800

    Added homepage and dashboard layouts

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const passport = require('passport');
+const exphps = require('express-handlebars');
 
 // Load User Model
 require('./models/User');
@@ -12,6 +13,7 @@ require('./config/passport')(passport);
 
 // Load Routes
 const auth = require('./routes/auth');
+const index = require('./routes/index');
 
 // Load Keys
 const keys = require('./config/keys');
@@ -20,21 +22,24 @@ const keys = require('./config/keys');
 mongoose.Promise = global.Promise;
 // Mongoose Connect
 mongoose.connect(keys.mongoURI)
-  .then(() => console.log('MongoDB Connected'))
-  .catch(err => console.log(err));
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err));
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('It Works!');
-});
+// Handlebars Middleware
+app.engine('handlebars', exphps({
+    defaultLayout: 'main'
+}));
+
+app.set('view engine', 'handlebars');
 
 // Authentication Middleware
 app.use(cookieParser());
 app.use(session({
-  secret: 'secret',
-  resave: false,
-  saveUninitialized: false
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: false
 }));
 
 // Passport middleware
@@ -42,16 +47,17 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 // Set Global Vars
-app.use((req, res, next)=>{
-  res.locals.user = req.user || null;
-  next();
+app.use((req, res, next) => {
+    res.locals.user = req.user || null;
+    next();
 });
 
 // Use Routes
+app.use('/', index);
 app.use('/auth', auth);
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-  console.log(`Server started on port ${port}`)
+    console.log(`Server started on port ${port}`)
 });
