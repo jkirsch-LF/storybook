a333ef61356ec3434aa5f1ec66e5e887554c58c1
James Kirsch <jameskirschaf@gmail.com>
2018-03-01 10:34:53 -0800

    Added functionality to view specific user's public stories

--- routes/stories.js
+++ routes/stories.js
@@ -9,7 +9,7 @@ const { ensureAuthenticated, ensureGuest } = require('../helpers/auth');
 router.get('/', (req, res) => {
     Story.find({ status: 'public' })
         .populate('user')
-        .sort({date:'desc'})
+        .sort({ date: 'desc' })
         .then(stories => {
             res.render('stories/index', {
                 stories: stories
@@ -25,11 +25,49 @@ router.get('/show/:id', (req, res) => {
         .populate('user')
         .populate('comments.commentUser')
         .then(story => {
-            res.render('stories/show', {
-                story: story
-            });
+            if (story.status == 'public') {
+                res.render('stories/show', {
+                    story: story
+                });
+            } else {
+                if(req.user){
+                    if(req.user.id == story.user._id){
+                        res.render('stories/show', {
+                            story: story
+                        });
+                    } else {
+                        res.redirect('/stories')
+                    }
+                } else {
+                    res.redirect('/stories')
+                }
+            }
         });
 });
+
+// List stories from a user
+router.get('/user/:userId', (req, res) => {
+    Story.find({ user: req.params.userId, status: 'public' })
+        .populate('user')
+        .then(stories => {
+            res.render('stories/index', {
+                stories: stories
+            });
+        })
+});
+
+// Llogged in users stories
+router.get('/my', ensureAuthenticated, (req, res) => {
+    Story.find({ user: req.user.id })
+        .populate('user')
+        .then(stories => {
+            res.render('stories/index', {
+                stories: stories
+            });
+        })
+});
+
+
 
 // Add Story Form
 router.get('/add', ensureAuthenticated, (req, res) => {
@@ -42,13 +80,13 @@ router.get('/edit/:id', ensureAuthenticated, (req, res) => {
         _id: req.params.id
     })
         .then(story => {
-            if(story.user != req.user.id){
+            if (story.user != req.user.id) {
                 res.redirect('/stories');
             } else {
                 res.render('stories/edit', {
                     story: story
                 });
-            } 
+            }
         });
 })
 
